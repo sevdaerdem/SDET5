5791fcca6d5f07b40309dfcee5f54075c382e457
Casper <sevdaerdem1@windowslive.com>
2023-01-31 23:09:35 +0300

    proje acıldı

--- src/Gun01/MainApp.java
+++ src/Gun01/MainApp.java
@@ -3,6 +3,11 @@ package Gun01;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("merhaba");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.techno.study/");
+
+
+
 
 
     }
